3ce4b8aaf86476a5b75803f429b09a0bf75c6faf
fesslerj <thefessler@gmail.com>
2018-10-26 08:57:14 -0700

    update 1

--- src/edu/calpoly/jvfessle/DoubleMath.java
+++ src/edu/calpoly/jvfessle/DoubleMath.java
@@ -9,43 +9,28 @@ import static java.lang.Math.rint;
 import java.math.BigInteger;
 import java.math.RoundingMode;
 import java.util.Iterator;
+import edu.calpoly.jvfessle.DoubleMathHelper;
 
 public final class DoubleMath {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
-		return;
-	}
-	
-	static boolean isFinite(double x) {
-		return x==0;
-	}
-	static boolean isMathematicalInteger(double x) {
-		return x==3.0;
-	}
-	static boolean checkRoundingUnnecessary(boolean x) {
-		return x;
-	}
-	
 	static double roundIntermediate(double x, RoundingMode mode) {
-	    if (!isFinite(x)) {
+	    if (!DoubleMathHelper.isFinite(x)) {
 	      throw new ArithmeticException("input is infinite or NaN");
 	    }
 	    switch (mode) {
 	      case UNNECESSARY:
-	        checkRoundingUnnecessary(isMathematicalInteger(x));
+	    	  DoubleMathHelper.checkRoundingUnnecessary(DoubleMathHelper.isMathematicalInteger(x));
 	        return x;
 
 	      case FLOOR:
-	        if (x >= 0.0 || isMathematicalInteger(x)) {
+	        if (x >= 0.0 || DoubleMathHelper.isMathematicalInteger(x)) {
 	          return x;
 	        } else {
 	          return (long) x - 1;
 	        }
 
 	      case CEILING:
-	        if (x <= 0.0 || isMathematicalInteger(x)) {
+	        if (x <= 0.0 || DoubleMathHelper.isMathematicalInteger(x)) {
 	          return x;
 	        } else {
 	          return (long) x + 1;
@@ -55,7 +40,7 @@ public final class DoubleMath {
 	        return x;
 
 	      case UP:
-	        if (isMathematicalInteger(x)) {
+	        if (DoubleMathHelper.isMathematicalInteger(x)) {
 	          return x;
 	        } else {
 	          return (long) x + (x > 0 ? 1 : -1);
---
+++ src/edu/calpoly/jvfessle/DoubleMathHelper.java
@@ -0,0 +1,19 @@
+package edu.calpoly.jvfessle;
+
+public final class DoubleMathHelper {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+		return;
+	}
+	
+	public static boolean isFinite(double x) {
+		return x==0;
+	}
+	public static boolean isMathematicalInteger(double x) {
+		return x==3.0;
+	}
+	public static boolean checkRoundingUnnecessary(boolean x) {
+		return x;
+	}
+}
